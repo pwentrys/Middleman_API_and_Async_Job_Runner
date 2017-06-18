677c3543f8e1b8118258197735bfaa18471263a4
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-06-18 13:08:38 -0400

    Virtualenv.

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-H:\Code\python_venv_361\Scripts\python.exe H:\Code\python\gappi_private\andromeda.py -dev
+H:\Code\python_envs\andromeda_361_win_64\Scripts\python.exe H:\Code\python\andromeda\andromeda.py -dev
