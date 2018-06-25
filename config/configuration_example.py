a70f351542a35ab34cb7bcfec33f33af45b9aac1
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2018-06-24 20:02:27 -0400

    Updated config example to include MySQL.

--- config/configuration_example.py
+++ config/configuration_example.py
@@ -1,5 +1,7 @@
 from .models.config import Config
 from .models.webenvs import WebEnvs
+from .models.sqlenvs import SQLEnvs
+from .credentials import SQL_USER, SQL_PASS, SQL_SCHEMA
 
 
 appname = f'GAPPI'
@@ -8,7 +10,7 @@ is_debug = False  # SUPER IMPORTANT
 
 config = Config({
     'name': appname,
-    'cookie_domain': f'{appnamelower}',
+    'cookie_domain': f'dev.localhost',  # f'{appnamelower}',
     'cookie_name': f'{appnamelower}_dev' if is_debug else f'{appnamelower}',
     'debug': is_debug,
     'web': WebEnvs({
@@ -27,4 +29,14 @@ config = Config({
             'lifetime': 1  # session lifetime in days.
         },
     }),
+    'sql': SQLEnvs({
+        'mysql': {
+            'USERNAME': SQL_USER,
+            'PASSWORD': SQL_PASS,
+            'DATABASE': SQL_SCHEMA,
+            'CONFIGS': '?charset=utf8',
+            'ADDRESS': '0.0.0.0',
+            'PORT': 3306,
+        }
+    })
 })
